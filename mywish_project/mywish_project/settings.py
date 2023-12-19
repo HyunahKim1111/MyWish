022e755ea3ed145e00b91e945a0f78601c073766
Stop silver <dnd5669@gmail.com>
2023-12-19 10:28:11 +0900

    메인페이지 완료

--- mywish_project/mywish_project/settings.py
+++ mywish_project/mywish_project/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,16 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+
+# 프론트 이미지 경로를 위해 추가함
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+# 프론트 이미지 경로를 위해 추가함
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
